3d08f725f5f8b7285cddc13a3684477f4d332671
sergio <frikiland2@gmail.com>
2015-02-20 18:03:05 +0100

    Revert "sdads"
    This reverts commit 4c0f763b16776852e99c0443d280f39f07a2a0e0.

--- model/businessLayer/prueba1.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function __autoload($nombre_clase) {
-    include 'Class_'.$nombre_clase . '.php';
-}
-$sergio =new Persona("090wrwec","Gonzalez","Sergio");
-echo $sergio-> getNombre();
-?>
--- view/error.php
+++
@@ -1,6 +0,0 @@
-ERROR
-
-<?php
-include "view/logout.php";
-?>
-<a href="../index.php">Inicio</a>
