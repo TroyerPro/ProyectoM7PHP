a8f54e7ab8da62f0ac071aff25655dc584cd0305
sergio <frikiland2@gmail.com>
2015-04-28 15:18:22 +0200

    bbdd nueva
    asdasd

--- bbdd/agencia.sql
+++ bbdd/agencia.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-03-2015 a las 13:20:10
+-- Tiempo de generación: 28-04-2015 a las 15:18:08
 -- Versión del servidor: 5.6.20
 -- Versión de PHP: 5.5.15
 
@@ -62,11 +62,40 @@ INSERT INTO `actor` (`DNI`, `nombre`, `apellidos`, `img`, `principal`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE IF NOT EXISTS `comentarios` (
+`id` int(11) NOT NULL,
+  `nombre` varchar(150) NOT NULL,
+  `comentario` varchar(300) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=24 ;
+
+--
+-- Volcado de datos para la tabla `comentarios`
+--
+
+INSERT INTO `comentarios` (`id`, `nombre`, `comentario`) VALUES
+(13, 'Adri', 'Yo opino que pino.'),
+(14, 'Marc', 'Hoy no vengo a clase porque puedo, fak you m12 xD'),
+(15, 'Olga', 'xzszcx'),
+(16, 'Olga', 'A ver si ya teneis tiempo de preparar la fiesta'),
+(17, 'Jairo', 'Adri homosexial'),
+(18, 'Jairo', 'Adri homosexial'),
+(19, 'XD', 'XD'),
+(20, 'Comentario', 'XD'),
+(21, 'xDD', 'dddd'),
+(22, 'xD', 'aasd'),
+(23, 'asdasd', 'asdas');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `director`
 --
 
 CREATE TABLE IF NOT EXISTS `director` (
-  `DNI` char(8) NOT NULL,
+  `NIF` char(8) NOT NULL,
   `nombre` char(20) NOT NULL,
   `apellidos` char(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -75,10 +104,9 @@ CREATE TABLE IF NOT EXISTS `director` (
 -- Volcado de datos para la tabla `director`
 --
 
-INSERT INTO `director` (`DNI`, `nombre`, `apellidos`) VALUES
-('555', 'Adrià', 'Mondéjar'),
-('33333', 'Sergio', 'Gonzalez'),
-('22222', 'Javier', 'Bufo');
+INSERT INTO `director` (`NIF`, `nombre`, `apellidos`) VALUES
+('48532', 'Adria', 'Mondejar'),
+('321', 'Sergio', 'Bufo');
 
 -- --------------------------------------------------------
 
@@ -95,7 +123,7 @@ CREATE TABLE IF NOT EXISTS `obra` (
   `actores_principales` char(100) NOT NULL,
   `actores_secundarios` char(100) NOT NULL,
   `director` char(20) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=31 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=65 ;
 
 --
 -- Volcado de datos para la tabla `obra`
@@ -131,7 +159,41 @@ INSERT INTO `obra` (`id_obra`, `nombre`, `genero`, `fecha_incio`, `fecha_final`,
 (27, '', 'Miedo', '0000-00-00', '0000-00-00', '	', '', ''),
 (28, '', 'Miedo', '0000-00-00', '0000-00-00', '	asd', '', ''),
 (29, '', 'Miedo', '0000-00-00', '0000-00-00', '	', '', ''),
-(30, '', 'Miedo', '0000-00-00', '0000-00-00', '	', '', '');
+(30, '', 'Miedo', '0000-00-00', '0000-00-00', '	', '', ''),
+(31, 'dasdsa', 'Aventura', '0000-00-00', '0000-00-00', 'dddd', '', ''),
+(32, 'Sergio Gonzalez Gomez', 'Miedo', '0000-00-00', '0000-00-00', 'Sergio', '13132', 'Sergio'),
+(33, 'saasd', 'Miedo', '0000-00-00', '0000-00-00', 'sda', 'dddd', 'Adria'),
+(34, 'Serj', 'Miedo', '0000-00-00', '0000-00-00', 'Raphael', '13132', 'Adria'),
+(35, 'saasd', 'Miedo', '0000-00-00', '0000-00-00', 'sda', 'dddd', 'Adria'),
+(36, 'saasd', 'Miedo', '0000-00-00', '0000-00-00', 'Array', 'Array', 'Adria'),
+(37, 'saasd', 'Miedo', '0000-00-00', '0000-00-00', 'Array', 'Array', 'Adria'),
+(38, 'saasd', 'Miedo', '0000-00-00', '0000-00-00', '', '', 'Adria'),
+(39, 'saasd', 'Miedo', '0000-00-00', '0000-00-00', '', '', 'Adria'),
+(40, 'asdsad', 'Miedo', '0000-00-00', '0000-00-00', 'Array', 'Array', 'Adria'),
+(41, 'asdsad', 'Miedo', '0000-00-00', '0000-00-00', 'Array', '', 'Adria'),
+(42, 'zsasdas', 'Miedo', '0000-00-00', '0000-00-00', '0', '0', 'Adria'),
+(43, 'asdsadadsasd', 'Miedo', '0000-00-00', '0000-00-00', '0', '0', 'Adria'),
+(44, 'asdsadadsasd', 'Miedo', '0000-00-00', '0000-00-00', '0', '0', 'Adria'),
+(45, 'sdasdsa', 'Suspense', '0000-00-00', '0000-00-00', '0', '0', 'Adria'),
+(46, 'sdasdsa', 'Suspense', '0000-00-00', '0000-00-00', '0', '0', 'Adria'),
+(47, 'sdasdsa', 'Suspense', '0000-00-00', '0000-00-00', '', '', 'Adria'),
+(48, 'sdasdsa', 'Suspense', '0000-00-00', '0000-00-00', '', '', 'Adria'),
+(49, 'sdasdsa', 'Suspense', '0000-00-00', '0000-00-00', ',13132', ',sad', 'Adria'),
+(50, 'sdasdsa', 'Suspense', '0000-00-00', '0000-00-00', ',13132', ',sad', 'Adria'),
+(51, 'sdasdsa', 'Suspense', '0000-00-00', '0000-00-00', ',13132', ',sad', 'Adria'),
+(52, 'sdasdsa', 'Suspense', '0000-00-00', '0000-00-00', ',13132', ',sad', 'Adria'),
+(53, 'sdasdsa', 'Suspense', '0000-00-00', '0000-00-00', ',13132', ',sad', 'Adria'),
+(54, 'sdasdsa', 'Suspense', '0000-00-00', '0000-00-00', '13132', 'sad', 'Adria'),
+(55, 'sdasdsa', 'Suspense', '0000-00-00', '0000-00-00', '0', '0', 'Adria'),
+(56, 'sdasdsa', 'Suspense', '0000-00-00', '0000-00-00', '0', '0', 'Adria'),
+(57, 'sdasdsa', 'Suspense', '0000-00-00', '0000-00-00', '0', '0', 'Adria'),
+(58, 'sdasdsa', 'Suspense', '0000-00-00', '0000-00-00', '13132', '0', 'Adria'),
+(59, 'sqdasdasd', 'Miedo', '0000-00-00', '0000-00-00', '0', '0', 'Adria'),
+(60, 'sqdasdasd', 'Miedo', '0000-00-00', '0000-00-00', 'Sergio,Raphael', 'Adri,ddddd', 'Adria'),
+(61, 'sqdasdasd', 'Miedo', '0000-00-00', '0000-00-00', 'Sergio,Raphael', 'Adri,ddddd', 'Adria'),
+(62, '', '', '0000-00-00', '0000-00-00', '', '', ''),
+(63, 'ObraCON MULTI', 'Miedo', '0000-00-00', '0000-00-00', 'Sergio,Raphael', 'Adri,ddddd', 'Adria'),
+(64, 'asdasdas', 'Miedo', '2015-03-19', '2015-03-20', '13132,Raphael', '13132', 'Adria');
 
 -- --------------------------------------------------------
 
@@ -156,6 +218,12 @@ INSERT INTO `users` (`username`, `password`) VALUES
 --
 
 --
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+ ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `obra`
 --
 ALTER TABLE `obra`
@@ -166,10 +234,15 @@ ALTER TABLE `obra`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24;
+--
 -- AUTO_INCREMENT de la tabla `obra`
 --
 ALTER TABLE `obra`
-MODIFY `id_obra` int(200) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=31;
+MODIFY `id_obra` int(200) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=65;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
